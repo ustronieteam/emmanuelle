91cac2f7f2751f113f73eb92383798cfce2e688c
marian.s27 <marian.s27@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-05 22:44:42 +0000

    zmiany nagłówklów dla zdalnych interfejsow

--- server/Headers/Observers/RemoteServerDisconnectedObserver.h
+++ server/Headers/Observers/RemoteServerDisconnectedObserver.h
@@ -8,7 +8,7 @@
 #include "ServerDataBase.h"
 #include "ServerRecord.h"
 #include "RemoteObserverData.h"
-#include "IServerServer.h"
+#include "IServerInterfaces.h"
 #include <boost/shared_ptr.hpp>
 #include <iostream>
 #include <exception>
