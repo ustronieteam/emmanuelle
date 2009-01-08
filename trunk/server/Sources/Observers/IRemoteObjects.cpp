e2e0a575cd5e77501c8da55f1b5b835a50502d3a
marian.s27 <marian.s27@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-08 23:10:07 +0000

    Dodanie implementacji klasy baqziowej dla objektu obserwowanego

--- trunk/server/Sources/Observers/IRemoteObjects.cpp
+++ trunk/server/Sources/Observers/IRemoteObjects.cpp
@@ -20,17 +20,32 @@ IRemoteObjects::~IRemoteObjects()
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int IRemoteObjects::Notify(RemoteObserverData observerData)
 {
+	for(std::vector<RemoteObserver *>::iterator it = RemoteObserversList.begin();
+		it != RemoteObserversList.end();	//Dopuki nie doszlismy do konca zbioru
+			it++)
+	{
+		(*it)->Refresh(observerData);
+	}
 	return 0;
 }
 
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int IRemoteObjects::UnregisterObserv(IRemoteObserver & observer)
 {
+	for(std::vector<RemoteObserver *>::const_iterator it = RemoteObserversList.begin();
+		it != RemoteObserversList.end();	//Dopuki nie doszlismy do konca zbioru
+			it++)
+	{
+		if((*(*it)) == observer)
+			break;
+	}
+	RemoteObserversList.erase(it);
 	return 0;
 }
 
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int IRemoteObjects::RegisterObserv( IRemoteObserver * observer)
 {
+	RemoteObserversList.push_back(observer);
 	return 0;
 }
