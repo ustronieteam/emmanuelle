5367b5f6b5add8fc03955de337d045eddb10c261
coutoPL <coutoPL@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-09 23:28:58 +0000

    zmiana polozenia katalogu z wygenerowanymmi plikami Corby (tak aby oba projekty mogly z nich korzystac jednoczesnie) a co za tym idzie zmiana sciezek includowanych plikow, zmiana parametrow zdalnej metody Connect (dodanie adresu)

---
+++ trunk/CorbaClasses/Headers/IServerClient/IServerClient_impl.h
@@ -0,0 +1,72 @@
+#ifndef ___IServerClient_impl_h__
+#define ___IServerClient_impl_h__
+
+
+#include <OB/CORBA.h>
+#include <IServerClient_skel.h>
+#include "IRemoteObjects.h"
+#include "RemoteObserverData.h"
+#include "EventType.h"
+#include "ServerRecord.h"
+#include "ClientRecord.h"
+
+//
+// IDL:IServerClient:1.0
+//
+class IServerClient_impl : virtual public POA_IServerClient,
+                           virtual public PortableServer::RefCountServantBase,
+						   virtual public IRemoteObjects
+{
+    IServerClient_impl(const IServerClient_impl&);
+    void operator=(const IServerClient_impl&);
+
+    PortableServer::POA_var poa_;
+
+public:
+
+    IServerClient_impl(PortableServer::POA_ptr);
+    ~IServerClient_impl();
+
+    virtual PortableServer::POA_ptr _default_POA();
+
+    //
+    // IDL:IServerClient/Connect:1.0
+    //
+    virtual ::DomainData::Address* Connect(const ::DomainData::Address& server,
+                                           ::DomainData::Mode m,
+                                           const ::DomainData::User& usr)
+        throw(::CORBA::SystemException);
+
+    //
+    // IDL:IServerClient/Disconnect:1.0
+    //
+    virtual void Disconnect()
+        throw(::CORBA::SystemException);
+
+    //
+    // IDL:IServerClient/Register:1.0
+    //
+    virtual ::DomainData::User* Register(const ::DomainData::User& usr)
+        throw(::CORBA::SystemException);
+
+    //
+    // IDL:IServerClient/SendMessage:1.0
+    //
+    virtual void SendMessage(const ::DomainData::Address& receiverAddress,
+                             const ::DomainData::Message& msg)
+        throw(::CORBA::SystemException);
+
+    //
+    // IDL:IServerClient/CheckClientStatus:1.0
+    //
+    virtual ::DomainData::Enability CheckClientStatus(const ::DomainData::Address& clientAddress)
+        throw(::CORBA::SystemException);
+
+    //
+    // IDL:IServerClient/GetPipeHolder:1.0
+    //
+    virtual ::DomainData::Address* GetPipeHolder(const ::DomainData::Address& receiverAddress)
+        throw(::CORBA::SystemException);
+};
+
+#endif
