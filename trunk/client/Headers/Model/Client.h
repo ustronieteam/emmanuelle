0cd3d67ea92d0e4a75a4a37b32e2e94721fb73d3
marian.s27 <marian.s27@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-10 14:22:04 +0000

    Klasa Client - commit na prosbe Couta

--- trunk/client/Headers/Model/Client.h
+++ trunk/client/Headers/Model/Client.h
@@ -1,46 +1,26 @@
 #ifndef CLIENT_H
 #define CLIENT_H
-//Begin section for file Client.h
-//TODO: Add definitions that you want preserved
-//End section for file Client.h
 
 #include "IRemoteObserver.h"
+#include "IServerClient.h"
 
-
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
+///@author Marian Szczykulski
+///@brief Model klienta jest
 class Client
 {
 
-    //Begin section for Client
-    //TODO: Add attributes that you want preserved
-    //End section for Client
-
-
+	private:
+		IServerClient_var connectedServerInstance; //Namiastka serwera do którego jestesmy pod³¹czeni
 
     public:
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         Client();
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual ~Client();
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         int AddFileObserver(IRemoteObserver & fileObserver);
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         int SendPackage();
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         int ConnectToServer();
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         int Disconnect();
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         int SendMessage();
-
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         int AddMessageObserver(IRemoteObserver & messageObserver);
 
 };  //end class Client
