39d030b6cabda7e676e05c78cdf9650d546ad5bc
marian.s27 <marian.s27@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-10 14:22:34 +0000

    Klasa Client commit na prośbe Couta

--- client/Sources/Model/Client.cpp
+++ client/Sources/Model/Client.cpp
@@ -1,59 +1,39 @@
 #include "Client.h"
-//Begin section for file Client.cpp
-//TODO: Add definitions that you want preserved
-//End section for file Client.cpp
-
-
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 Client::Client() 
 {
-    //TODO Auto-generated method stub
+
 }
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 Client::~Client() 
 {
-    //TODO Auto-generated method stub
+
 }
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int Client::AddFileObserver(IRemoteObserver & fileObserver) 
 {
-    //TODO Auto-generated method stub
     return 0;
 }
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int Client::SendPackage() 
 {
-    //TODO Auto-generated method stub
     return 0;
 }
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int Client::ConnectToServer() 
 {
-    //TODO Auto-generated method stub
     return 0;
 }
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int Client::Disconnect() 
 {
-    //TODO Auto-generated method stub
     return 0;
 }
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int Client::SendMessage() 
 {
-    //TODO Auto-generated method stub
     return 0;
 }
-
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 int Client::AddMessageObserver(IRemoteObserver & messageObserver) 
 {
-    //TODO Auto-generated method stub
     return 0;
 }
