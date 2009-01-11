5afd52d7a633440764126039642422abf9bec55f
coutoPL <coutoPL@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-11 02:12:13 +0000

    nowsza wersja funkcji wczytujacej pliki (z uzyciem boost/regex)

--- client/Headers/Aplication.h
+++ client/Headers/Aplication.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <boost/regex.hpp>
 
 #include "IModel.h"
 #include "Model.h"
--- client/Headers/Model/IModel.h
+++ client/Headers/Model/IModel.h
@@ -1,62 +1,41 @@
 #ifndef IMODEL_H
 #define IMODEL_H
-//Begin section for file IModel.h
-//TODO: Add definitions that you want preserved
-//End section for file IModel.h
+
 
 #include "DataObserver.h"
 #include "FilePackage.h"
 #include "IRemoteObserver.h"
 
 
-//@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 class IModel
 {
 
-    //Begin section for IModel
-    //TODO: Add attributes that you want preserved
-    //End section for IModel
-
-
-
     public:
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         IModel();
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual ~IModel();
 
 		virtual void SetServerAddress(const char * addr) = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int AddStatusObserver(DataObserver & observer) = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int SendPackage(FilePackage package) = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int DeleteContact() = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         //virtual int SendMessage() = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int AddContact() = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int Disconnect() = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual bool ConnectToServer() = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int AddFileObserver(IRemoteObserver & observer) = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual int AddMessageObserver(IRemoteObserver & observer) = 0;
 
-        //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual  int GetContactsList() = 0; // TODO: zmienic typ zwracany
 
 };  //end class IModel
