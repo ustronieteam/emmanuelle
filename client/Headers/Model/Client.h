79aacc70098931b1045412d2d862cc032b32b239
marian.s27 <marian.s27@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-11 00:40:02 +0000

    Dodanie wczytywania adresu z pliku konfiguracyjnego

--- client/Headers/Model/Client.h
+++ client/Headers/Model/Client.h
@@ -3,15 +3,38 @@
 
 #include "IRemoteObserver.h"
 #include "IServerClient.h"
+//Do Tworzenia logow
+#include <log4cxx/logger.h>
+#include <log4cxx/level.h>
+//Do pliku konfiguracyjnego
+#include <boost/program_options.hpp>
+#include <string>
+#include <fstream>
+namespace po = boost::program_options;
 
+///
 ///@author Marian Szczykulski
-///@brief Model klienta jest
+///@date	2009-01-10
+///@brief	Model klienta zapewnia komunikacje sieciowa, poprzez objekt CORBY
+///
 class Client
 {
 
 	private:
+		static const std::string configFileName;
+		///
+		///Namiastka serwera
 		IServerClient_var connectedServerInstance; //Namiastka serwera do którego jestesmy pod³¹czeni
 
+		///
+		///Adres zdalnego serwera do ktorego podlaczamy sie
+		DomainData::Address serverAddress;
+
+		// logger
+		log4cxx::LoggerPtr logger;
+
+		//
+		void readServerAddress();
     public:
 
         Client();
@@ -22,6 +45,8 @@ class Client
         int Disconnect();
         int SendMessage();
         int AddMessageObserver(IRemoteObserver & messageObserver);
+		void setServerAddress(std::string address) {}//TODO dokonczyc
+		std::string ServerAddress() { return serverAddress.localization.in();}//TODO dokonczyc
 
 };  //end class Client
 
--- client/Sources/Model/Client.cpp
+++ client/Sources/Model/Client.cpp
@@ -1,14 +1,51 @@
 #include "Client.h"
+
+const std::string Client::configFileName = "client.config";
+///
+///@author Marian Szczykulski
+///@date 2009-01-11
+///@brief Konstruktor klienta
 Client::Client() 
 {
-
+	logger = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("Client"));
+	logger->setLevel(log4cxx::Level::getAll());		
+	readServerAddress();
 }
 
 Client::~Client() 
 {
 
 }
+///
+///@author Marian Szczykulski
+///@date 2009-01-11
+///@brief czyta adres serwera z pliku konfiguracyjnego
+void Client::readServerAddress()
+{
+	std::string servAddr="";
+	po::options_description configOption( "Opcje pliku konfiguracyjnego" );
+	configOption.add_options()
+			   ("serverAddress", po::value<std::string>(&servAddr),
+     "Adres Ip servera domyslnego") 
+	;
+	po::variables_map vm;
+	std::ifstream file;
+	file.open(configFileName.c_str());
 
+	try
+	{
+		store( parse_config_file(file,configOption), vm );
+		po::notify( vm );
+		serverAddress.localization.out() = CORBA::string_dup(servAddr.c_str());//???
+		file.close();
+	}
+	catch(std::exception &exc)
+	{
+		LOG4CXX_ERROR(logger, "Blad podczas parsowania pliku konfiguracyjnego. Wyjatek: "<<exc.what() ); 
+		file.close();
+	}
+	
+}
 int Client::AddFileObserver(IRemoteObserver & fileObserver) 
 {
     return 0;
@@ -18,9 +55,20 @@ int Client::SendPackage()
 {
     return 0;
 }
-
+///
+///@author Marian Szczykulski
+///@date	2009-01-10
+///@brief	Metoda umo¿liwiajaca pod³¹czenie sie klienta do danego serwera
 int Client::ConnectToServer() 
 {
+	if(serverAddress.localization.in() != NULL && strcmp(serverAddress.localization.in(),"")!=0)
+	{//Adres jest ustawiony
+
+	}
+	else
+	{//Rzuc wyjatek nie mozna podlaczys sie do serwera
+
+	}
     return 0;
 }
 
