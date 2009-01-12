b49ec5425555ca5f7799cff48f0a0e9e6aa83823
marian.s27 <marian.s27@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-12 23:24:46 +0000

    Dodanie funktora mnasluchu zmiany statusu (poprawka)

--- client/Headers/Model/StatusCheckerFunctor.h
+++ client/Headers/Model/StatusCheckerFunctor.h
@@ -4,18 +4,25 @@
 #include <boost/shared_ptr.hpp>
 
 #include "ClientsData.h"
+#include "Client.h"
+
+#include <vector>
+
 #include <log4cxx/logger.h>
 #include <log4cxx/level.h>
 
 class StatusCheckerFunctor
 {
 	private:
+		static const int sleepTime = 10000;
 		log4cxx::LoggerPtr logger;
 		boost::shared_ptr<ClientsData> clientsData;
+		boost::shared_ptr<Client> _client;
 	public:
-		StatusCheckerFunctor(boost::shared_ptr<ClientsData> clData)
+		StatusCheckerFunctor(boost::shared_ptr<ClientsData> clData, boost::shared_ptr<Client> client)
 		{
 			clientsData = clData;
+			_client = client;
 			logger = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("StatusCheckerFunctor"));
 			logger->setLevel(log4cxx::Level::getAll());
 		}
