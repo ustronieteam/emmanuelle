ffbe17d1eb6c19ccc205333bb1e6a9a414db2f56
w.grzeskowiak <w.grzeskowiak@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-13 02:44:28 +0000

    Dodanie logow do ServerDataBase ;p

--- trunk/server/Sources/DataBase/ServerDataBase.cpp
+++ trunk/server/Sources/DataBase/ServerDataBase.cpp
@@ -4,7 +4,9 @@
 /// Konstruktor bezparametrowy.
 ServerDataBase::ServerDataBase() 
 {
-
+	// Inicjalizacja loggera.
+	_logger = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("ServerDataBase"));
+	_logger->setLevel(log4cxx::Level::getAll());
 }
 
 ///
@@ -54,12 +56,17 @@ const ServerRecord & ServerDataBase::GetRecord(int recordId)
 {
 	boost::mutex::scoped_lock sl(_mutex);
 
+	LOG4CXX_DEBUG(_logger, "Pobieranie rekordu o id: " << recordId);
+
 	if ( this->_records.count(recordId) != 0 )
 	{
+		LOG4CXX_DEBUG(_logger, "Znaleziono rekord. Address.localization: " << _records[recordId].GetAddress().localization.in() );
 		return _records[recordId];
 	}
 	else
 	{
+		LOG4CXX_ERROR(_logger, "Nie znaleziono rekordu.");
+
 		std::exception ex("Brak rekordu o podanym ID!");
 		throw ex;
 	}
@@ -72,6 +79,8 @@ std::vector<ServerRecord> ServerDataBase::GetAllRecords()
 {
 	boost::mutex::scoped_lock sl(_mutex);
 	std::vector<ServerRecord> v;
+
+	LOG4CXX_DEBUG(_logger, "Pobieranie wszystkich rekordow.");
 	
 	if ( _records.size() != 0 )
 		for(std::map<int, ServerRecord>::iterator i = this->_records.begin(); i != this->_records.end(); i++)
@@ -88,6 +97,8 @@ int ServerDataBase::InsertRecord(const ServerRecord & record)
 {
 	boost::mutex::scoped_lock sl(_mutex);
 
+	LOG4CXX_DEBUG(_logger, "Wstawianie rekordu. Address.localization: " << record.GetAddress().localization.in() );
+
 	if ( this->_records.count(record.GetRecordId()) == 0 )
 	{
 		this->_records[record.GetRecordId()] = record;
@@ -95,6 +106,8 @@ int ServerDataBase::InsertRecord(const ServerRecord & record)
 	}
 	else
 	{
+		LOG4CXX_ERROR(_logger, "Rekord o podanym id juz istnieje");
+
 		std::exception ex("Record o podanym ID jest juz w bazie danych!");
 		throw ex;
 	}
@@ -108,13 +121,20 @@ int ServerDataBase::DeleteRecord(int recordId)
 {
 	boost::mutex::scoped_lock sl(_mutex);
 
+	LOG4CXX_DEBUG(_logger, "Usuwanie rekordu o id: " << recordId );
+
 	if ( this->_records.count(recordId) != 0 )
 	{
 		this->_records.erase(recordId);
+
+		LOG4CXX_DEBUG(_logger, "Rekord usunieto." );
+
 		return 1;
 	}
 	else
 	{
+		LOG4CXX_ERROR(_logger, "Nie znaleziono rekordu!" );
+
 		std::exception ex("Brak rekordu o podanym ID!");
 		throw ex;
 	}
@@ -128,13 +148,20 @@ int ServerDataBase::ModifyRecord(const ServerRecord & record)
 {
 	boost::mutex::scoped_lock sl(_mutex);
 
+	LOG4CXX_DEBUG(_logger, "Modyfikacja rekordu. (new)Address.localization: " << record.GetAddress().localization.in() );
+
 	if ( this->_records.count(record.GetRecordId()) != 0 )
 	{
 		_records[record.GetRecordId()] = record;
+
+		LOG4CXX_DEBUG(_logger, "Rekord zmodyfikowano.");
+
 		return 1;
 	}
 	else
 	{
+		LOG4CXX_ERROR(_logger, "Nie zmodyfikowano rekordu!");
+
 		std::exception ex("Brak rekordu o podanym ID!");
 		throw ex;
 	}
@@ -181,11 +208,17 @@ int ServerDataBase::Find(const struct DomainData::Address & address)
 {
 	boost::mutex::scoped_lock sl(_mutex);
 
+	LOG4CXX_DEBUG(_logger, "Szukanie po strukturze Address. Address.name: " << address.name.in() << " Address.localization: " << address.localization.in() );
+
 	if ( _records.size() != 0 )
 		for(std::map<int, ServerRecord>::iterator i = this->_records.begin(); i != this->_records.end(); i++)
 			if ( strcmp((*i).second.GetAddress().localization.in(), address.localization.in()) == 0 )
+			{
+				LOG4CXX_DEBUG(_logger, "Znaleziono rekord!");
 				return (*i).second.GetRecordId();
+			}
 
+	LOG4CXX_DEBUG(_logger, "Nie znaleziono rekordu!");
 	return -1;
 }
 
