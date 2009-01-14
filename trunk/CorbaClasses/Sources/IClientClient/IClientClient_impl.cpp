c87c0aced5217d5d51b343d594aa6875e37a9976
coutoPL <coutoPL@c118a9a8-d993-11dd-9042-6d746b85d38b>
2009-01-14 01:57:46 +0000

    dodanie logow do uprzednio dodanych metod zdalnych

--- trunk/CorbaClasses/Sources/IClientClient/IClientClient_impl.cpp
+++ trunk/CorbaClasses/Sources/IClientClient/IClientClient_impl.cpp
@@ -30,6 +30,8 @@ IClientClient_impl::SendFile(const ::DomainData::File& f,
                              const ::DomainData::User& receiver)
     throw(::CORBA::SystemException)
 {
+	std::cout << "WYWOLANIE SENDFILE" << std::endl;
+
 	LOG4CXX_DEBUG(logger, "WYWOLANIE SENDFILE");
 
 	RemoteObserverData observData;
